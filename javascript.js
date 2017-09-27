6e6964538c673be27383647e63e8745904b9476f
Joseph DiPeri <jdiperi88@gmail.com>
2017-09-27 12:20:27 -0400

    added drum machine sounds and sounds folder

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 console.log('connected');
+
+window.addEventListener('keydown',function(event){
+    console.log(event);
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+    console.log(audio);
+    if(!audio) return;
+    audio.currentTime =0;
+    audio.play();
+    key.classList.add('playing');
+});
