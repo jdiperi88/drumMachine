bc5f14e8aaf28b282e2b09dd3f34e649d9ba15d6
Joseph DiPeri <jdiperi88@gmail.com>
2017-10-04 22:39:01 -0400

    added style to all of the buttons and changed background to black

--- javascript.js
+++ javascript.js
@@ -3,6 +3,19 @@ console.log('connected');
 window.addEventListener('keydown',function(event){
     console.log(event);
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    // const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+    console.log(audio);
+    if(!audio) return;
+    audio.currentTime =0;
+    audio.play();
+    key.classList.add('playing');
+});
+
+window.addEventListener('click',function(event){
+    console.log(event);
+    const audio = document.querySelector(`audio[data-key="${event}"]`)
+    // const audio = document.querySelector(`#s`)
+    console.log(audio)
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
     console.log(audio);
     if(!audio) return;
