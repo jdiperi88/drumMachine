c3ff84f36cbcadcfed0ebba4b273fb65619bfad9
Joseph DiPeri <jdiperi88@gmail.com>
2017-10-28 00:36:15 -0400

    added onclicks to make it mouse and mobile friendly

--- javascript.js
+++ javascript.js
@@ -11,16 +11,15 @@ window.addEventListener('keydown',function(event){
     key.classList.add('playing');
 });
 
-// window.addEventListener('click',function(event){
-//     console.log(event);
-//     const clicked = document.getElementById('')
-//     const audio = document.querySelector(`audio[data-key="${event}"]`)
-//     // const audio = document.querySelector(`#s`)
-//     console.log(audio)
-//     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
-//     console.log(audio);
-//     if(!audio) return;
-//     audio.currentTime =0;
-//     audio.play();
-//     key.classList.add('playing');
-// });
+window.addEventListener('click',function(event){
+    console.log(event.target.id);
+    const audio = document.querySelector(`#${event.target.id}Audio`)
+    // const audio = document.querySelector(`#s`).innerHTML
+    console.log(audio)
+    // const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+    console.log(audio);
+    if(!audio) return;
+    audio.currentTime =0;
+    audio.play();
+    // key.classList.add('playing');
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 #Drum Machine
+
+
+#Links/Resouces 
+* https://courses.wesbos.com/account/access/59c947187b1edc674255a75f/view/6cf22385d8
